aa5a8f2b09472a6d36792a7a3cdd9dc9e4b5610a
sergey <ootieroo@gmail.com>
2022-07-06 02:34:56 +0400

    пока 2 задачи

---
+++ task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 19
+ int n,r,sum=0,temp;    
+ Console.Write("Введите пятизначное число: ");   
+ n = int.Parse(Console.ReadLine());  
+ temp=n;      
+ while(n>0)      
+ {      
+ r=n%10;      
+ sum=(sum*10)+r;      
+ n=n/10;      
+ }      
+ if(temp==sum)      
+ Console.Write("Число является полиндромом.");      
+ else      
+ Console.Write("Число не является полиндромом.");     
+    
---
+++ task02/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача 21
+Console.WriteLine("Введите координаты первой точки:");
+// int x1, y1 = Convert.ToInt32(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double newPoint()
+{
+double newPoint = Math.Sqrt(Math.Pow(x2-x1, 2)+ Math.Pow(y2-y1, 2)+ Math.Pow(z2-z1, 2));
+return newPoint;
+}
+
+Console.WriteLine(newPoint());
+
